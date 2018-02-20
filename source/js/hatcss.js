abea34936c79f69f6f1530561f8b293143a92572
jasiel.macedo <jasielmacedo@gmail.com>
2018-02-20 19:30:42 -0300

    Fix js and add animation example

--- source/js/hatcss.js
+++ source/js/hatcss.js
@@ -1,3 +1,12 @@
+/*!
+ * Hatcss -https://github.com/jasielmacedo/hatcss/
+ * Version - 1.0.0
+ * Licensed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2018 Jasiel Macedo, Stefano Mendes, Gisele Mendes, André Zapalá
+ */
 jQuery(function(){
-	if(0!==jQuery.fn.waypoint){jQuery(".animate_when_almost_visible:not(.start_animation)").waypoint(function(){jQuery(this).addClass("start_animation animated")}}
+	jQuery(".animate_when_almost_visible:not(.start_animation)").waypoint(function(){
+		jQuery(this.element).addClass("start_animation").addClass("animated");		
+	},{offset: '100%'});
 });
